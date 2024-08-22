42a8e717be886959e778a916ab3047fdd6b625a0
LucasBD <jlucasbucalon@gmail.com>
2024-08-21 22:19:19 -0300

    final do projeto

--- script.js
+++ script.js
@@ -3,16 +3,16 @@ const particlesContainer = document.getElementById("particles-container");
 function createParticle() {
   const particle = document.createElement("div");
   particle.classList.add("particle");
-  particle.style.top = `${particlesContainer.offsetHeight}px`; // cria a partícula na parte inferior da tela
-  particle.style.left = `${Math.random() * particlesContainer.offsetWidth}px`; // posição aleatória em x
+  particle.style.top = `${Math.random() * window.innerHeight}px`;
+  particle.style.left = `${Math.random() * window.innerWidth}px`;
   particle.style.transform = `translate(0, 0)`;
-  particle.velocityX = Math.random() * 2 - 1; // velocidade aleatória em x
-  particle.velocityY = -Math.random() * 2 + 1; // velocidade para cima (negativa)
+  particle.velocityX = Math.random() * 2 - 1;
+  particle.velocityY = Math.random() * 2 - 1;
   particlesContainer.appendChild(particle);
 }
 
 function animateParticles() {
-  const particles = particlesContainer.children;
+  const particles = document.querySelectorAll(".particle");
   for (let i = 0; i < particles.length; i++) {
     const particle = particles[i];
     particle.style.top = `${
@@ -21,9 +21,7 @@ function animateParticles() {
     particle.style.left = `${
       parseFloat(particle.style.left) + particle.velocityX
     }px`;
-    // atualiza a posição da partícula com base na sua velocidade
 
-    // verifica se a partícula saiu da tela e a reposiciona
     if (
       parseFloat(particle.style.top) < 0 ||
       parseFloat(particle.style.top) > particlesContainer.offsetHeight
